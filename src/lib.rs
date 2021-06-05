2633b30b8f18d819318b84495a27c799aaf055a0
Nathan Files <nathanwfiles@gmail.com>
2021-06-05 18:28:09 -0400

    problem 2

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate log;
 mod tests {
     use super::*;
     use log::info;
+    use std::iter::repeat_with;
     use std::sync::Once;
 
     static INIT: Once = Once::new();
@@ -22,5 +23,25 @@ mod tests {
         let sum: u32 = (1..MAX).filter(|i| i % 3 == 0 || i % 5 == 0).sum();
 
         info!("problem 001: {}", sum);
+    }
+
+    #[test]
+    fn problem002() {
+        init();
+
+        const MAX: u32 = 4_000_000;
+        let mut a: u32 = 0;
+        let mut b: u32 = 1;
+        let total: u32 = repeat_with(|| {
+            let next = a + b;
+            a = b;
+            b = next;
+            next
+        })
+        .take_while(|&num| num <= MAX)
+        .filter(|&num| num % 2 == 0)
+        .sum();
+
+        info!("problem 002: {}", total);
     }
 }
