19d9a633f616774ad770a8904fd2196373990b73
Nathan Files <nathanwfiles@gmail.com>
2021-06-05 18:34:37 -0400

    adds comments

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ mod tests {
         INIT.call_once(env_logger::init);
     }
 
+    /// Find the sum of all the multiples of 3 or 5 below 1000.
     #[test]
     fn problem001() {
         init();
@@ -25,6 +26,8 @@ mod tests {
         info!("problem 001: {}", sum);
     }
 
+    /// By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+    /// find the sum of the even-valued terms.
     #[test]
     fn problem002() {
         init();
