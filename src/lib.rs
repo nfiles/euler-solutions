3a48c444195c43e1b16d65950a8987f64f79f53e
Nathan Files <nathanwfiles@gmail.com>
2021-06-05 18:51:18 -0400

    problem 3

--- src/lib.rs
+++ src/lib.rs
@@ -47,4 +47,24 @@ mod tests {
 
         info!("problem 002: {}", total);
     }
+
+    /// What is the largest prime factor of the number 600851475143 ?
+    #[test]
+    fn problem003() {
+        init();
+
+        let mut num: u64 = 600851475143;
+        let mut factor: u64 = 1;
+        for n in 2.. {
+            while num % n == 0 {
+                factor = n;
+                num /= n;
+            }
+            if num == 1 {
+                break;
+            }
+        }
+
+        info!("problem 003: {}", factor);
+    }
 }
