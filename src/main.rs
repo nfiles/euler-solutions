65bc7463e14dc0227805439d78e7985d6f777432
Nathan Files <nathanwfiles@gmail.com>
2022-01-09 23:48:14 -0500

    progress report

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use clap::{App, Arg};
 mod solutions;
 
 const PROBLEM: &str = "number";
+const TIMING: &str = "timing";
 
 fn main() {
     env_logger::init();
@@ -21,13 +22,28 @@ fn main() {
         .arg(
             Arg::with_name(PROBLEM)
                 .value_name("problem")
-                .required(true)
                 .help("number of the project euler solution"),
+        )
+        .arg(
+            Arg::with_name(TIMING)
+                .long("time")
+                .takes_value(false)
+                .help("report how long it took to find the solution"),
         )
         .setting(clap::AppSettings::AllowLeadingHyphen)
         .get_matches();
 
-    let number = match matches.value_of(PROBLEM).unwrap().parse::<u32>() {
+    let timing = matches.is_present(TIMING);
+
+    let number_arg = match matches.value_of(PROBLEM) {
+        None => {
+            print!("{}", solutions::progress());
+            std::process::exit(0);
+        }
+        Some(number_str) => number_str,
+    };
+
+    let number = match number_arg.parse::<u32>() {
         Err(err) => {
             eprintln!("Error parsing the number: {}", err);
             std::process::exit(1);
@@ -38,6 +54,7 @@ fn main() {
     let start = std::time::Instant::now();
     let solution = solutions::solve(number);
     let elapsed = start.elapsed();
+
     match solution {
         None => {
             eprintln!("no solution for problem {}", number);
@@ -45,7 +62,9 @@ fn main() {
         }
         Some(result) => {
             println!("problem {}: {}", number, result);
-            println!("completed in {} seconds", elapsed.as_secs_f32());
+            if timing {
+                println!("completed in {} seconds", elapsed.as_secs_f32());
+            }
         }
     }
 }
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,5 +1,8 @@
 extern crate itertools;
 
+use std::collections::HashMap;
+use std::fmt::Write;
+
 pub mod problem001;
 pub mod problem002;
 pub mod problem003;
@@ -7,16 +10,61 @@ pub mod problem049;
 pub mod problem050;
 pub mod problem051;
 
-pub fn solve(num: u32) -> Option<String> {
-    let solution = match num {
-        1 => problem001::run(),
-        2 => problem002::run(),
-        3 => problem003::run(),
-        49 => problem049::run(),
-        50 => problem050::run(),
-        51 => problem051::run(),
-        _ => return None,
-    };
+pub type Solver = fn() -> String;
 
-    Some(solution)
+pub fn all_solutions() -> HashMap<u32, Solver> {
+    let mut solutions: HashMap<u32, Solver> = HashMap::new();
+
+    solutions.insert(1, problem001::run);
+    solutions.insert(2, problem002::run);
+    solutions.insert(3, problem003::run);
+    solutions.insert(49, problem049::run);
+    solutions.insert(50, problem050::run);
+    solutions.insert(51, problem051::run);
+
+    solutions
+}
+
+pub fn solve(num: u32) -> Option<String> {
+    let solutions = all_solutions();
+    let solution = solutions.get(&num);
+
+    solution.map(|solver| solver())
+}
+
+pub fn progress() -> String {
+    let solutions = all_solutions();
+    let max_solution = *solutions.keys().max().unwrap_or(&0);
+    let num_width = String::from(format!("{}", max_solution)).len();
+
+    let row_len = 10;
+
+    let mut buffer = String::new();
+
+    writeln!(&mut buffer, "Project Euler solutions").unwrap();
+
+    for row in 0..=(max_solution / row_len) {
+        for col in 0..row_len {
+            let num = row * row_len + col + 1;
+            let solution = solutions.get(&num);
+            write!(
+                &mut buffer,
+                "{: >width$}: [{}]",
+                num,
+                match solution {
+                    Some(_) => "X",
+                    None => " ",
+                },
+                width = num_width,
+            )
+            .unwrap();
+
+            if col + 1 < row_len {
+                write!(&mut buffer, "  ").unwrap();
+            }
+        }
+        writeln!(&mut buffer).unwrap();
+    }
+
+    buffer
 }
